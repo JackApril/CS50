fb68bdb6d123303d198154609d62d1a3ee5958e7
vaish01_aditya-web <vaish01aditya@gmail.com>
2026-06-24 16:07:27 +0530

    Firts Commit!

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# num1= int(input("Enter the first number:"))
+# num2=int(input("Enter the second number:"))
+# # if you have'nt typecasted the input type it will take string and output will be 12 if input is 1 and 2
+# # to get output as 3 let num3=int(num1)+int(num2)
+# num3= num1+num2;
+# print(num3, end="\t")
+# num3=num2-num1 
+# print(num3, end="\t")
+# num3=num1*num2 
+# print(num3, end="\t")
+# num3=num2/float(num1)
+# print(num3, end="\t")
+x=float(input("What's in x?"))
+y=float(input("What's in y?"))
+z=round(x+y)
+print(f"{z:,}")
